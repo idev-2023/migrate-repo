70c7363a62af7cf383c7ad9993d255df3d5aa08a
idev-2023 <cyvyard2019@gmail.com>
2023-06-16 10:05:06 -0700

    uodated main

--- main.tf
+++ main.tf
@@ -1,13 +1,4 @@
 terraform {
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.3.2"
-    }
-  }
-  required_version = ">= 1.1.0"
-}
-
   cloud {
     organization = "terraform-cloud-org1"
 
@@ -15,6 +6,16 @@ terraform {
       name = "production"
     }
   }
+
+  required_version = ">= 1.1.0"
+
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }  
+}
 
 
 resource "aws_s3_bucket" "main" {
