9290d6c5c94ffcf06660807719c3aec9d94c7fbd
idev-2023 <cyvyard2019@gmail.com>
2023-06-16 10:06:47 -0700

    updated

--- main.tf
+++ main.tf
@@ -7,12 +7,12 @@ terraform {
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.3.6"
 
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "3.3.2"
+      version = "4.6.0"
     }
   }  
 }
