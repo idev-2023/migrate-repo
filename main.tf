48adfd1f583ba48470411d1edb06161a10610394
idev-2023 <cyvyard2019@gmail.com>
2023-06-16 10:01:25 -0700

    updated main

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.3.2"
+    }
+  }
+  required_version = ">= 1.1.0"
+}
+
   cloud {
     organization = "terraform-cloud-org1"
 
@@ -6,7 +15,7 @@ terraform {
       name = "production"
     }
   }
- }
+
 
 resource "aws_s3_bucket" "main" {
   bucket_prefix = "migrate-me"
